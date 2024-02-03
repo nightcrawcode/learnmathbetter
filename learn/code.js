293805fb95d62449dfba44c3ba49d4bb78830702
Martin <143912975+nightcrawcode@users.noreply.github.com>
2024-02-03 05:47:22 +0000

    testing

---
+++ learn/code.js
@@ -0,0 +1,10 @@
+/*
+Right now, JS is hard
+and may take a bit to work with
+however i will be testing a few things
+with it before megeing onto the main
+hope this help for now.
+*/
+
+document.getElementById("demo").innerHTML = 5 + 6;
+document.getElementById("demo").innerHTML = "this is cool"; 
